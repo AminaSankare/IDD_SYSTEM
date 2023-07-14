77336edc0468e0a85d42d35784cea9b80bd1e8aa
nkurudavid <nkurudavid44@gmail.com>
2023-07-15 00:50:05 +0200

    registering models

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+# register models
+admin.sites.register(Citizen)
+admin.sites.register(CitizenAddress)
+admin.sites.register(CitizenDocument)
+admin.sites.register(CitizenParents)
+admin.sites.register(DocumentApplication)
