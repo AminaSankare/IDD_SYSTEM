1c49dc644b26c18b614cbb59832102725d029c85
nkurudavid <nkurudavid44@gmail.com>
2023-07-15 00:52:41 +0200

    done with registering models to admin panel

--- management/admin.py
+++ management/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . models import *
 
 # register models
-admin.sites.register(Citizen)
-admin.sites.register(CitizenAddress)
-admin.sites.register(CitizenDocument)
-admin.sites.register(CitizenParents)
-admin.sites.register(DocumentApplication)
+admin.site.register(Citizen)
+admin.site.register(CitizenAddress)
+admin.site.register(CitizenDocument)
+admin.site.register(CitizenParents)
+admin.site.register(DocumentApplication)
