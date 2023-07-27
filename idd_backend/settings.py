39c67e797aa94fb554425a812b00dc3b1d4b43ad
nkurudavid <nkurudavid44@gmail.com>
2023-07-27 22:18:35 +0200

    modified content

--- idd_backend/settings.py
+++ idd_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t=be3u+wehx&o))5j+yc0x2un^clw+87qldnf3at4f7sf9oq-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -149,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# statc file directory
+# static file directory
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
@@ -175,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # #gmail_send/settings.py
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'solvitoutsourcingmanagementsys@gmail.com'
+# EMAIL_HOST_USER = 'managementsys@gmail.com'
 # EMAIL_HOST_PASSWORD = 'acgbsiwvxjbqjtgj' #passwd app here
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
